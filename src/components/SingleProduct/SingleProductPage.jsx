2aeccd9dca6e56bed4525b60981654f831823562
iSalah-YT <82966610+iSalah-YT@users.noreply.github.com>
2023-09-16 09:14:00 +0100

    Update backend URL

--- src/components/SingleProduct/SingleProductPage.jsx
+++ src/components/SingleProduct/SingleProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 
 import './SingleProductPage.css';
+import config from '../../config.json';
 import QuantityInput from './QuantityInput';
 import { useParams } from 'react-router-dom';
 import useData from '../../hooks/useData';
@@ -28,7 +29,7 @@ const SingleProductPage = () => {
               {product.images.map((image, index) => (
                 <img
                   key={index}
-                  src={`http://localhost:8080/products/${image}`}
+                  src={`${config.backendURL}/products/${image}`}
                   alt={product.title}
                   className={selectedImage === index ? 'selected_image' : ''}
                   onClick={() => setSelectedImage(index)}
